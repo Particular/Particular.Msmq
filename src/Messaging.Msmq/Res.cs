9fe79361efd35426eec3b7788d2f7fe6ad6b5dab
Brandon Ording <bording@gmail.com>
2023-10-04 17:08:24 -0400

    WIP

---
+++ src/Messaging.Msmq/Res.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Messaging
+{
+    using System.Collections.Generic;
+
+    static class Res
+    {
+        static string GetString(string id) => strings[id];
+
+        static readonly Dictionary<string, string> strings = new();
+    }
+}
+
