61492653d0f5988ceb4b4e9577e38fc5bfa91436
Brandon Ording <bording@gmail.com>
2023-10-04 17:54:45 -0400

    WIP

--- src/Messaging.Msmq/System.Messaging/Message.cs
+++ src/Messaging.Msmq/System.Messaging/Message.cs
@@ -1072,10 +1072,6 @@ namespace System.Messaging
                     //This property has not been set, lets return an empty queue.
                     if (!receiveCreated)
                     {
-                        if (LocalAppContextSwitches.UseMD5ForDefaultHashAlgorithm)
-                        {
-                            return HashAlgorithm.Md5;
-                        }
                         return HashAlgorithm.Sha512;
                     }
 
