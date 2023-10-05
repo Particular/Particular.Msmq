59060bdd81ac18a35ad9ce360981d49e881cca0b
Brandon Ording <bording@gmail.com>
2023-10-05 12:04:19 -0400

    Add API approval test

---
+++ src/Tests/APIApprovals.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests
+{
+    using Messaging.Msmq;
+    using NUnit.Framework;
+    using Particular.Approvals;
+    using PublicApiGenerator;
+
+    [TestFixture]
+    public class APIApprovals
+    {
+        [Test]
+        public void ApproveApi()
+        {
+            var publicApi = typeof(MessageQueue).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                ExcludeAttributes = ["System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute"]
+            });
+
+            Approver.Verify(publicApi);
+        }
+    }
+}
